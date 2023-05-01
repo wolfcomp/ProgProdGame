25944e4a284423dda2dd0bb1adb11cdf4b361689
brukernavnD <90253215+brukernavnD@users.noreply.github.com>
2023-05-01 10:20:40 +0200

    added some changes to ai

--- Source/BackRowStudio/Private/Wolf.cpp
+++ Source/BackRowStudio/Private/Wolf.cpp
@@ -3,95 +3,62 @@
 
 #include "Wolf.h"
 
-#include "MainCharacter.h"
-#include "WolfAIController.h"
-#include "Components/SplineComponent.h"
-#include "Components/SphereComponent.h"
-#include "Components/BoxComponent.h"
 #include "Animation/AnimMontage.h"
 #include "BehaviorTree/BehaviorTreeComponent.h"
 #include "BehaviorTree/BlackboardComponent.h"
+#include "Components/BoxComponent.h"
+#include "Components/SphereComponent.h"
+#include "Components/SplineComponent.h"
 #include "GameFramework/CharacterMovementComponent.h"
+#include "MainCharacter.h"
+#include "WolfAIController.h"
 
 // Sets default values
 AWolf::AWolf()
 {
- 	// Set this character to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
-	PrimaryActorTick.bCanEverTick = true;
+    // Set this character to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+    PrimaryActorTick.bCanEverTick = true;
 
-	PlayerAttackCollisionDetection = CreateDefaultSubobject<USphereComponent>(TEXT("Attempt Attack Range Visual"));
-	PlayerAttackCollisionDetection->SetupAttachment(RootComponent);
-	PatrolPath = CreateDefaultSubobject<USplineComponent>(TEXT("Wolf Patrol Path"));
-	PatrolPath->SetupAttachment(RootComponent);
-	//WolfAiBlackboard = CreateDefaultSubobject<UBlackboardComponent>(TEXT("Wolf Blackboard Component"));
-	//WolfAiBehaviorTree = CreateDefaultSubobject<UBehaviorTreeComponent>(TEXT("BehaviorTreeComponent"));
-	AttackHitBox = CreateDefaultSubobject<UBoxComponent>(TEXT("Wolf Attack Hitbox"));
-	AttackHitBox->SetupAttachment(RootComponent);
+    PlayerAttackCollisionDetection = CreateDefaultSubobject<USphereComponent>(TEXT("Attempt Attack Range Visual"));
+    PlayerAttackCollisionDetection->SetupAttachment(RootComponent);
+    PatrolPath = CreateDefaultSubobject<USplineComponent>(TEXT("Wolf Patrol Path"));
+    PatrolPath->SetupAttachment(RootComponent);
+    AttackHitBox = CreateDefaultSubobject<UBoxComponent>(TEXT("Wolf Attack Hitbox"));
+    AttackHitBox->SetupAttachment(RootComponent);
 
-	//setting up character movement
-	GetCharacterMovement()->MaxWalkSpeed = 600.0f;
-	GetCharacterMovement()->bOrientRotationToMovement = true;
-
+    // setting up character movement
+    GetCharacterMovement()->MaxWalkSpeed = 600.0f;
+    GetCharacterMovement()->bOrientRotationToMovement = true;
 }
 
 // Called when the game starts or when spawned
 void AWolf::BeginPlay()
 {
-	if(AWolfAIController *tempCon = Cast<AWolfAIController>(GetController()))
-	{
-	    WolfAIController = tempCon;
-		//WolfAIController->GetPathFollowingComponent()->OnRequestFinished.AddUObject
-	 //   (this, &AWolf::OnMoveCompleted);
-	}
+    if (AWolfAIController *tempCon = Cast<AWolfAIController>(GetController()))
+    {
+        WolfAIController = tempCon;
+    }
 
-	AnimInstance = GetMesh()->GetAnimInstance();
-	//AttackHitBox->OnComponentBeginOverlap.AddDynamic(this, &AWolf::OnAttackHitBoxBeginOverlap);
+    AnimInstance = GetMesh()->GetAnimInstance();
 
-	Super::BeginPlay();
-	
+    Super::BeginPlay();
 }
 
 // Called every frame
-void AWolf::Tick(float DeltaTime)
-{
-	Super::Tick(DeltaTime);
-
-}
+void AWolf::Tick(float DeltaTime) { Super::Tick(DeltaTime); }
 
 // Called to bind functionality to input
-void AWolf::SetupPlayerInputComponent(UInputComponent* PlayerInputComponent)
-{
-	Super::SetupPlayerInputComponent(PlayerInputComponent);
+void AWolf::SetupPlayerInputComponent(UInputComponent *PlayerInputComponent) { Super::SetupPlayerInputComponent(PlayerInputComponent); }
 
-}
+// just in case we wanna do something like play an animation
+void AWolf::OnMoveCompleted(FAIRequestID RequestID, const FPathFollowingResult &Result) {}
 
-//just in case we wanna do something like play an animation
-void AWolf::OnMoveCompleted(FAIRequestID RequestID, const FPathFollowingResult &Result)
-{
-	//GEngine->AddOnScreenDebugMessage(-1, 3.f, FColor::Yellow, TEXT("I just finished moving today"));
-}
-
-void AWolf::AttackAnimationEnded()
-{
-	WolfAIController->Reactivate();
-}
+void AWolf::AttackAnimationEnded() { WolfAIController->Reactivate(); }
 
 void AWolf::TryAttack(AActor *actorToAttack)
 {
-	if(AttackMontage)
-	{
-	    AnimInstance->Montage_Play(AttackMontage);   
-	}
+    if (AttackMontage)
+    {
+        AnimInstance->Montage_Play(AttackMontage);
+    }
 }
-
-void AWolf::OnAttackHitBoxBeginOverlap(UPrimitiveComponent *OverlappedComponent, AActor *OtherActor, UPrimitiveComponent *OtherComp, int32 OtherBodyIndex, bool bFromSweep, const FHitResult &SweepResult)
-{
-	PlayerRef = Cast<AMainCharacter>(OtherActor);
-	if (PlayerRef && CanDealDamage)
-	{
-		// deal damage to player
-		UE_LOG(LogTemp, Warning, TEXT("Player Damaged"));
-		GEngine->AddOnScreenDebugMessage(-1, 3.f, FColor::Yellow, TEXT("Insert 3 paragraph evil monologue here"));
-	}
-}
-
--- Source/BackRowStudio/Public/WolfAIController.h
+++ Source/BackRowStudio/Public/WolfAIController.h
@@ -2,81 +2,86 @@
 
 #pragma once
 
-#include "CoreMinimal.h"
 #include "AIController.h"
+#include "CoreMinimal.h"
 #include "WolfAIController.generated.h"
 
-//helpful tutorial: https://www.youtube.com/watch?v=DwjehZh5YQ0&t=119s
-//also this other helpful tutorial: https://sologamedevblog.com/tutorials/unreal-perception-c-friend-or-enemy/
-    //The other controllers need to inherit from IGenericTeamAgentInterface and set their team id. In this example I take the PlayerController
+// helpful tutorial: https://www.youtube.com/watch?v=DwjehZh5YQ0&t=119s
+// also this other helpful tutorial: https://sologamedevblog.com/tutorials/unreal-perception-c-friend-or-enemy/
+// The other controllers need to inherit from IGenericTeamAgentInterface and set their team id. In this example I take the PlayerController
 
 /**
- * 
+ *
  */
 UCLASS()
 class BACKROWSTUDIO_API AWolfAIController : public AAIController
 {
-	GENERATED_BODY()
+    GENERATED_BODY()
+
 protected:
     virtual void BeginPlay() override;
-	virtual void Tick(float DeltaSeconds) override;
+
+    virtual void Tick(float DeltaSeconds) override;
 
 private:
-	APawn* PlayerPawn;
+    APawn *PlayerPawn;
 
-	class AWolf* controlledWolf;
+    class AWolf *controlledWolf;
 
-	class UNavigationSystemV1* NavArea;
+    class UNavigationSystemV1 *NavArea;
 
-	FVector RandomLocation = FVector();
-	TArray<FVector> patrolPoints;
+    FVector RandomLocation = FVector();
 
-	bool bIsActive;
-	bool bMoveToPlayer;
-	bool bSearchForPlayer;
-	bool bCanAttackPlayer;
+    TArray<FVector> patrolPoints;
 
-	float wolfAttackDistance = 200.0f;
-	float currentPatrolPoint = 0;
+    bool bIsActive;
+    bool bSearchForPlayer;
 
-	void GenerateRandomSearchLocation();
-	void SearchForPlayer();
+    bool bMoveToPlayer;
 
-	void MoveToPlayer();
-	void StartChasingPlayer();
-	bool PlayerInAttackRange() const;
-	bool IsPointReachable(FVector point) const;
-	void AttackPlayer() const;
-	void Patrol();
+    void MoveToPlayer();
+    void StartChasingPlayer();
+    bool PlayerInAttackRange() const;
+    bool IsPointReachable(FVector point) const;
+    void AttackPlayer() const;
+    void Patrol();
 
-	FTimerHandle SearchTimerHandle;
-	FTimerHandle AttackTimerHandle;
+    bool bCanAttackPlayer;
+
+    float wolfAttackDistance = 200.0f;
+
+    float currentPatrolPoint = 0;
+
+    void GenerateRandomSearchLocation();
+
+    void SearchForPlayer();
 
 public:
-	AWolfAIController();
+    AWolfAIController();
 
-	UPROPERTY(VisibleAnywhere, Category = AI)
-	TObjectPtr<UAIPerceptionComponent> AIPerceptionComponent = nullptr;
-	TObjectPtr<class UAISenseConfig_Sight> AISenseConfigSight = nullptr;
-	TObjectPtr<class UAISenseConfig_Hearing> AISenseConfigHearing = nullptr;
+    UPROPERTY(VisibleAnywhere, Category = AI)
+    TObjectPtr<UAIPerceptionComponent> AIPerceptionComponent = nullptr;
 
-	UPROPERTY(EditAnywhere)
-	bool IsPatrolling = true;
+    TObjectPtr<class UAISenseConfig_Sight> AISenseConfigSight = nullptr;
 
-	UPROPERTY(EditAnywhere)
-	bool IsInitialized = true;
+    TObjectPtr<class UAISenseConfig_Hearing> AISenseConfigHearing = nullptr;
+
+    UPROPERTY(EditAnywhere)
+    bool IsPatrolling = true;
+
+    UPROPERTY(EditAnywhere)
+    bool IsInitialized = true;
 
     virtual void OnMoveCompleted(FAIRequestID RequestID, const FPathFollowingResult &Result) override;
-	virtual ETeamAttitude::Type GetTeamAttitudeTowards(const AActor& Other) const override;
+
+    virtual ETeamAttitude::Type GetTeamAttitudeTowards(const AActor &Other) const override;
 
     UFUNCTION()
-	void OnTargetPerceptionUpdated_Delegate(AActor* Actor, FAIStimulus Stimulus);
+    void OnTargetPerceptionUpdated_Delegate(AActor *Actor, FAIStimulus Stimulus);
 
-	UFUNCTION()
-	void Reactivate();
+    UFUNCTION()
+    void Reactivate();
 
-	UFUNCTION(BlueprintCallable)
-	void ReturnToPatrol();
-
-
+    UFUNCTION(BlueprintCallable)
+    void ReturnToPatrol();
 };
