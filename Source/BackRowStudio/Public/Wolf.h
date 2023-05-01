541e8b4cad1b4c88178fa2c9334c19870c888d09
brukernavnD <90253215+brukernavnD@users.noreply.github.com>
2023-05-01 16:33:49 +0200

    small changes

--- Source/BackRowStudio/Public/Wolf.h
+++ Source/BackRowStudio/Public/Wolf.h
@@ -36,10 +36,10 @@ public:
     // Called to bind functionality to input
     virtual void SetupPlayerInputComponent(class UInputComponent *PlayerInputComponent) override;
 
-    UPROPERTY(EditAnywhere)
+    UPROPERTY()
     class AMainCharacter *PlayerRef;
 
-    UPROPERTY(EditAnywhere)
+    UPROPERTY()
     class AWolfAIController *WolfAIController;
 
     UPROPERTY(EditAnywhere, BlueprintReadWrite)
