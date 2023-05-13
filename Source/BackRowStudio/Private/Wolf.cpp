173b44eae973312aa98b68ae2ebf3a0d727e075f
brukernavnD <90253215+brukernavnD@users.noreply.github.com>
2023-05-13 13:57:30 +0200

    formatting again

--- Source/BackRowStudio/Private/Wolf.cpp
+++ Source/BackRowStudio/Private/Wolf.cpp
@@ -14,7 +14,6 @@
 // Sets default values
 AWolf::AWolf()
 {
-	// Set this character to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
 	PrimaryActorTick.bCanEverTick = true;
 
 	PatrolPath = CreateDefaultSubobject<USplineComponent>(TEXT("Wolf Patrol Path"));
